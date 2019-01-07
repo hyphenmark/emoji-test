6587459a4c3c1ac06a478b7c650e46562a55ff0f
Amal Raghav <amal.raghav@gmail.com>
2019-01-07 21:02:58 +0530

    Base code for emoji test

---
+++ main.js
@@ -0,0 +1,19 @@
+/**
+ * Note: Your code has to work only on the latest stable version of Chrome browser. Please avoid spending time fixing other browser quirks.
+ * Also, please take care of the following conditions ...
+ * 1. Replace :) and <3 with the respective images in img folder
+ * 2. Once the text has been replaced with the image the text caret should be reset to the right of the image,
+ *    so that the user can continue typing.
+ *
+ * If you have any questions please don't hesitate to ask.
+ *
+ * Good luck! :)
+ */
+(function(window, emojis) {
+  var el = document.getElementById('user-input');
+
+  // space for your code magic
+})(window, {
+  ':)': 'img/smiling-face.png',
+  '<3': 'img/heart-red.png'
+});
